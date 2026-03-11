c7a5c3d397c63cfecd37003e2885231b3f1ed68e
HyerimKimm <helim01033@naver.com>
2026-03-11 23:08:21 +0900

    Refactor. delete useless codes

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 
-import { useState } from "react";
 import { HomeDashboard } from "@/pages/screens/HomeDashboard";
 import "@/shared/styles/app.scss";
-
-type Screen = "home" | "check" | "chat";
 
 export default function DashboardPage() {
   return <HomeDashboard />;
