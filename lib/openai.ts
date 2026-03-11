6abbffa477135e5055a8e7365a2c460fa95fd100
kimhyerim <kimhr4@visang.com>
2026-03-11 22:19:13 +0900

    Feat. openai 키 추가

---
+++ lib/openai.ts
@@ -0,0 +1,9 @@
+import OpenAI from "openai";
+
+if (!process.env.NEXT_PUBLIC_OPENAI_API_KEY) {
+  throw new Error("OPENAI_API_KEY가 환경 변수에 설정되어 있지 않습니다.");
+}
+
+export const openai = new OpenAI({
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+});
