c40912fdd7eba26d68ecaa86baee4e6571dbf56c
HyerimKimm <helim01033@naver.com>
2026-03-19 17:54:36 +0900

    Feat. 토스트 띄우는 기능 추가 / 토스트 UI 수정

--- shared/query/mood-entries/useAddTodayMutation.ts
+++ shared/query/mood-entries/useAddTodayMutation.ts
@@ -1,11 +1,14 @@
 import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
 import { useAuthStore } from '@/shared/store/useAuthStore';
+import { useToast } from '@/shared/ui/toast';
 
 /** 오늘의 기분 기록 추가 */
 const useAddTodayMutation = () => {
   const profile = useAuthStore((state) => state.profile);
   const session = useAuthStore((state) => state.session);
+
+  const showToast = useToast();
 
   return useMutation({
     mutationFn: async (variables: { emotions: string; energyLevel: string; thoughts: string }) => {
@@ -28,6 +31,12 @@ const useAddTodayMutation = () => {
           : undefined
       );
       return response.data;
+    },
+    onSuccess: () => {
+      showToast('success', '오늘의 기분 기록이 추가되었습니다.');
+    },
+    onError: (e) => {
+      showToast('error', '오늘의 기분 기록 추가에 실패했습니다.');
     },
   });
 };
--- shared/query/mood-entries/useUpdateTodayMutation.ts
+++ shared/query/mood-entries/useUpdateTodayMutation.ts
@@ -1,11 +1,14 @@
 import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
 import { useAuthStore } from '@/shared/store/useAuthStore';
+import { useToast } from '@/shared/ui/toast';
 
 /** 오늘의 기분 기록 수정 */
 const useUpdateTodayMutation = () => {
   const profile = useAuthStore((state) => state.profile);
   const session = useAuthStore((state) => state.session);
+
+  const showToast = useToast();
 
   return useMutation({
     mutationFn: async (variables: { emotions: string; energyLevel: string; thoughts: string }) => {
@@ -28,6 +31,12 @@ const useUpdateTodayMutation = () => {
           : undefined
       );
       return response.data;
+    },
+    onSuccess: () => {
+      showToast('success', '오늘의 기분 기록이 수정되었습니다.');
+    },
+    onError: (e) => {
+      showToast('error', '오늘의 기분 기록 수정에 실패했습니다.');
     },
   });
 };
