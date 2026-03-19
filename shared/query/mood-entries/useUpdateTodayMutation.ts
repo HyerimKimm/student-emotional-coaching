f44de3d44e224278d27c8caecba154012b0636b1
HyerimKimm <helim01033@naver.com>
2026-03-19 15:23:33 +0900

    Feat. 기분 기록 수정시에도 jwt 토큰 보내도록 기능 추가

--- shared/query/mood-entries/useUpdateTodayMutation.ts
+++ shared/query/mood-entries/useUpdateTodayMutation.ts
@@ -5,15 +5,28 @@ import { useAuthStore } from '@/shared/store/useAuthStore';
 /** 오늘의 기분 기록 수정 */
 const useUpdateTodayMutation = () => {
   const profile = useAuthStore((state) => state.profile);
+  const session = useAuthStore((state) => state.session);
 
   return useMutation({
     mutationFn: async (variables: { emotions: string; energyLevel: string; thoughts: string }) => {
-      const response = await axios.put(`/api/mood-entries/today`, {
-        studentId: profile?.id,
-        emotions: variables.emotions,
-        energyLevel: variables.energyLevel,
-        thoughts: variables.thoughts,
-      });
+      const accessToken = session?.access_token;
+
+      const response = await axios.put(
+        `/api/mood-entries/today`,
+        {
+          studentId: profile?.id,
+          emotions: variables.emotions,
+          energyLevel: variables.energyLevel,
+          thoughts: variables.thoughts,
+        },
+        accessToken
+          ? {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+              },
+            }
+          : undefined
+      );
       return response.data;
     },
   });
