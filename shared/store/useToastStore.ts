fd44493ccb2b69e2dd69dd5680814a17c4faaad7
HyerimKimm <helim01033@naver.com>
2026-03-19 17:25:31 +0900

    Feat. ToastStore 추가

--- shared/store/useToastStore.ts
+++ shared/store/useToastStore.ts
@@ -9,3 +9,39 @@ type ToastType = {
   state: ToastStateType;
   message: string;
 };
+
+type ToastStoreType = {
+  toastList: ToastType[];
+  show: (state: ToastStateType, message: string) => void;
+  close: () => void;
+};
+
+const store: StateCreator<ToastStoreType> = (set, get) => ({
+  toastList: [],
+
+  show: (state: ToastStateType, message: string) => {
+    const uniqueKey = `${dayjs().valueOf()}-${get().toastList.length}`;
+
+    const newToast = {
+      id: uniqueKey, // 고유한 ID 생성
+      state: state,
+      message: message,
+    };
+
+    set({ toastList: [...get().toastList, newToast] });
+
+    setTimeout(() => {
+      get().close();
+    }, 3000);
+  },
+
+  close: () => {
+    set({
+      toastList: get().toastList.slice(1),
+    });
+  },
+});
+
+const useToastStore = create(devtools(store, { name: 'toastStore' }));
+
+export default useToastStore;
