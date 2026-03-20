0f0994157301f259f06e40b2276d65a53019c706
HyerimKimm <helim01033@naver.com>
2026-03-20 15:57:12 +0900

    Feat. 로딩오버레이 추가

--- widget/loading_overay/LoadingOveray.tsx
+++ widget/loading_overay/LoadingOveray.tsx
@@ -1,13 +1,14 @@
 'use client';
 
-import { useIsFetching } from '@tanstack/react-query';
+import { useIsFetching, useIsMutating } from '@tanstack/react-query';
 import styles from './LoadingOveray.module.scss';
 import LoadingIcon from '@/shared/asset/icons/LoadingIcon';
 
 export default function LoadingOveray() {
   const isFetching = useIsFetching();
+  const isMutating = useIsMutating();
 
-  if (isFetching > 0) {
+  if (isFetching > 0 || isMutating > 0) {
     return (
       <div className={styles.loading_wrap}>
         <LoadingIcon />
